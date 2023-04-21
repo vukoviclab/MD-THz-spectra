07c08798d15efe1193dc64f03e18d2e19b263135
Payam Kelich <61854834+pkelich@users.noreply.github.com>
2023-04-21 13:17:49 -0600

    Update z-contact-area-dop-index.sh

--- locallized-region/z-contact-area-dop-index.sh
+++ locallized-region/z-contact-area-dop-index.sh
@@ -6,7 +6,7 @@ for i in {1..10}
 	sed -i "s/set p.*/set p \[atomselect top \"resname CNT and (same residue as resname CNT and within 8 of segname D$i)\"]/g" z-contact-index-dop-ref.tcl
 	vmd -dispdev text -e z-contact-index-dop-ref.tcl
 	
-	indexes=`/home/payam/.virtualenvs/ml/bin/python cindex.py z-index-dop-wD$i.csv 4000`
+	indexes=`python cindex.py z-index-dop-wD$i.csv 4000`
 
 
 	sed -i "s/set outfile.*/set outfile [open z-contact-dop-wD$i.csv w]/g" z-contact-area-ref.tcl
